40ac4cab654f8907caef05f2e9c20bc8420e83f9
User <user@MB033361.local>
2024-07-02 16:39:32 -0300

    play sound when button is pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    // MARK: - PROPERTIES
+    var player: AVAudioPlayer!
+    
+    // MARK: - VIEW LIFE CYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    // MARK: - ACTIONS
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
     
+    // MARK: - FUNCTIONS
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
 
